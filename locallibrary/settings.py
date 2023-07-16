954ccb0c67fbf4f7856a15a0ab9a737345baf178
viltt-0266 <le.thi.tuong.vi@sun-asterisk.com>
2023-07-16 19:26:57 +0700

    Added files and changes required for deployment cu?i

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os # needed by code below
-
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -81,10 +81,7 @@ WSGI_APPLICATION = 'locallibrary.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default='postgresql://postgres:MTqa7WDjmlBsNqpY73Og@containers-us-west-168.railway.app:7865/railway')
 }
 
 
