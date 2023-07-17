a548e7410a52e3c12f2553d04ee7e6ee9c11d347
viltt-0266 <le.thi.tuong.vi@sun-asterisk.com>
2023-07-17 10:38:40 +0700

    Update

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'ZUa2Q4hVgIGmWDT6_mRmjDnqfM8Xrl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["https://vimysite-production.up.railway.app",'https://*.127.0.0.1']
+ALLOWED_HOSTS = ["vimysite-production.up.railway.app",'https://*.127.0.0.1']
 CSRF_TRUSTED_ORIGINS = ["https://vimysite-production.up.railway.app",'https://*.127.0.0.1']
 
 CORS_ALLOWED_ORIGINS = [
