878f0e9c9757a197c22746d9b841e08aea7c4cf3
viltt-0266 <le.thi.tuong.vi@sun-asterisk.com>
2023-07-17 10:08:22 +0700

    Update

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -24,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = "cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag"
 
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'ZUa2Q4hVgIGmWDT6_mRmjDnqfM8Xrl2u7lDw4cCYbfo')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://vimysite-production.up.railway.app']
+ALLOWED_HOSTS = ['vimysite-production.up.railway.app']
 
 CORS_ALLOWED_ORIGINS = [
     "https://vimysite-production.up.railway.app",
